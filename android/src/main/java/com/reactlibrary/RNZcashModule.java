39cd357364938bde616b12bcec938541c32fc0a8
Shawn Cook <sfncook@gmail.com>
2019-10-24 15:48:20 -0700

    Android working

--- android/src/main/java/com/reactlibrary/RNZcashModule.java
+++ android/src/main/java/com/reactlibrary/RNZcashModule.java
@@ -5,6 +5,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactMethod;
 
 public class RNZcashModule extends ReactContextBaseJavaModule {
 
