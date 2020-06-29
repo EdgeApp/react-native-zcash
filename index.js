9aff77fa220d7115c7c995c83ab238b47b964579
Shawn Cook <sfncook@gmail.com>
2020-06-29 16:56:32 -0700

    Working?

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-import { NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 const { RNZcash } = NativeModules;
 
 export async function getNumTransactions (n) {
   return await RNZcash.getNumTransactions(n)
+}
+
+export async function subscribeToFoo (callback) {
+  const eventEmitter = new NativeEventEmitter(RNZcash);
+  const subscription = eventEmitter.addListener('FooEvent', callback);
 }
 
